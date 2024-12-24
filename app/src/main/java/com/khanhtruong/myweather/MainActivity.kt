b034eaba4a35066513a4e6f8e74cad6124295c0a
Kyle Truong <kyle.truong@linecorp.com>
2024-12-24 15:40:03 +0700

    Update UI and add hilt app

--- app/src/main/java/com/khanhtruong/myweather/MainActivity.kt
+++ app/src/main/java/com/khanhtruong/myweather/MainActivity.kt
@@ -4,14 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.khanhtruong.myweather.ui.theme.MyweatherTheme
+import com.khanhtruong.myweather.ui.screen.MyWeatherScreen
+import com.khanhtruong.myweather.ui.theme.MyWeatherTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,7 +14,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyweatherTheme {
+            MyWeatherTheme {
+                MyWeatherScreen()
             }
         }
     }
